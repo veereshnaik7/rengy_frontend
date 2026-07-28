a5bb00a60f41d039b18299d3606595fa9fafc9b4
Veeresh <jerryveeru1@gmail.com>
2026-07-29 00:22:12 +0530

    upadted prod api url

--- src/features/auth/authApi.ts
+++ src/features/auth/authApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:3000/api"}/auth`,
+  baseURL: `${import.meta.env.VITE_API_URL || "https://rengy-backend-nrla.onrender.com/api"}/auth`,
   withCredentials: true,
 });
 
